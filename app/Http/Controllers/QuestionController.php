a2958839c02c6c8ff83f86c0a18c9fee06c67a1a
DeBrito <brunordebrito@gmail.com>
2024-02-14 17:58:41 -0300

    HEY-9: it should check if ends with question mark ?

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+
+use Closure;
 use Illuminate\Http\{RedirectResponse, Request, Response};
 
 /**
@@ -23,7 +25,15 @@ class QuestionController extends Controller
     {
 
         $attributes = request()->validate([
-            'question' => ['required', 'min:10'],
+            'question' => [
+                'required',
+                'min:10',
+                function ($attribute, $value, Closure $fail) {
+                    if (substr($value, -1) !== '?') {
+                        $fail('Are you sure that is a question? It is missing a question mark in the end.');
+                    }
+                },
+            ],
         ]);
 
         Question::query()->create($attributes);
--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -34,6 +34,27 @@ it('should be able to create a new question bigger than 255 characters', functio
 
 it('should check if ends with question mark ?', function () {
 
+    /* Arrange :: Preparando */
+    $user = User::factory()->create();
+
+    actingAs($user);
+
+    /* action :: Agir */
+    $request = post(
+        route('question.store'),
+        [
+            'question' => str_repeat('*', 10),
+        ]
+    );
+
+    /* Assert :: Verificar */
+    $request->assertSessionHasErrors(
+        [
+            'question' => 'Are you sure that is a question? It is missing a question mark in the end.',
+        ]
+    );
+    assertDatabaseCount('questions', 0);
+
 });
 
 it('shoul have at least 10 characters', function () {
@@ -53,10 +74,11 @@ it('shoul have at least 10 characters', function () {
 
     /* Assert :: Verificar */
     $request->assertSessionHasErrors(
-        ['question' => __(
-            'validation.min.string',
-            ['min' => 10, 'attribute' => 'question']
-        ),
+        [
+            'question' => __(
+                'validation.min.string',
+                ['min' => 10, 'attribute' => 'question']
+            ),
         ]
     );
     assertDatabaseCount('questions', 0);
