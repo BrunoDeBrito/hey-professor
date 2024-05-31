c0694e9a911178f72d8e8ffc7cd04303835b4f18
TI <ti@federalst.com.br>
2024-05-31 16:44:03 -0300

    hey-11: adding like butgton to our interface

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,10 +2,41 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Question extends Model
 {
     use HasFactory;
+    /**
+     * Relationship with Vote
+     *
+     * @return HasMany<Vote>
+     */
+    public function votes(): HasMany
+    {
+        return $this->hasMany(Vote::class);
+    }
+
+    /**
+     * Calculate the total likes
+     *
+     * @return Attribute
+     */
+    public function likes(): Attribute
+    {
+        return new Attribute(get: fn () => $this->votes()->sum('like'));
+    }
+
+    /**
+     * Calculate the total unlikes
+     *
+     * @return Attribute
+     */
+    public function unlikes(): Attribute
+    {
+        return new Attribute(get: fn () => $this->votes()->sum('unlike'));
+    }
 }
--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -1,7 +1,26 @@
-@props([
-    'item'
-])
+@props(['item'])
 
-<div class="rounded dark:bg-gray-800/50 shadow shadow-blue-500/50 p-3 dark:gray-400">
-    {{ $item->question }}
+<div
+    class="
+    rounded dark:bg-gray-800/50 shadow shadow-blue-500/50 p-3
+    dark:gray-400 flex justify-between items-center">
+    <span>
+        {{ $item->question }}
+    </span>
+    <div>
+        <x-form :action="route('question.like', $item)">
+            <button class="flex items-start space-x-1 text-green-500 hover:text-green-300 cursor-pointer">
+                <x-icons.thumbs-up class=" w-5 h-5" id="thumb-up" />
+                <span>{{ $item->likes ?? '-' }}</span>
+            </button>
+        </x-form>
+        <x-form :action="route('question.like', $item)" id="form-unlike-{{ $item->id }}">
+            <hr class="border-gray-700 my-1">
+            <abutton
+                class="flex items-start space-x-1 text-red-500 hover:text-red-300 cursor-pointer">
+                <x-icons.thumbs-down class="w-5 h-5" id="thumb-down" />
+                <span>{{ $item->unlikes ?? '-' }}</span>
+            </a>
+        </x-form>
+    </div>
 </div>
