4dbc8e45b7c39274b626b3e23048d492f9e2fd33
TI <ti@federalst.com.br>
2024-07-01 09:58:29 -0300

    hey-24: apply filter in frontend

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Question;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
 
 /**
  * Controller responsible for the dashboard
@@ -18,7 +19,12 @@ class DashboardController extends Controller
     {
 
         $data = [
-            'questions' => Question::withSum('votes', 'like')
+            'questions' => Question::query()
+            ->when(request()->has('search'), function (Builder $query) {
+                $query->where('question', 'like', '%' . request()->search . '%');
+
+            })
+            ->withSum('votes', 'like')
                 ->withSum('votes', 'unlike')
                 ->orderByRaw('
                     case when (SELECT sum(votes.like) FROM votes WHERE questions.id = votes.question_id) is null then 0 else (SELECT sum(votes.like) FROM votes WHERE questions.id = votes.question_id) end desc,
